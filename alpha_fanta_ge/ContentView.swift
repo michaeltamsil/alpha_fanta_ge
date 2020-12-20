ee62b4fd429e421db766a1fb32445f5cf3480197
Michael Tamsil <michaeltamsil@gmail.com>
2020-12-20 20:52:49 +0700

    Initial Commit

---
+++ alpha_fanta_ge/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  alpha_fanta_ge
+//
+//  Created by michael tamsil on 20/12/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ alpha_fanta_ge/alpha_fanta_geApp.swift
@@ -0,0 +1,17 @@
+//
+//  alpha_fanta_geApp.swift
+//  alpha_fanta_ge
+//
+//  Created by michael tamsil on 20/12/20.
+//
+
+import SwiftUI
+
+@main
+struct alpha_fanta_geApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
