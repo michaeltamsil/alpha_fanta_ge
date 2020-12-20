bad8adcbcffe52b11a1cc0af7b4189fd051dba2e
Michael Tamsil <michaeltamsil@gmail.com>
2020-12-21 03:57:21 +0700

    create empty second view

---
+++ alpha_fanta_ge/SecondScreenView.swift
@@ -0,0 +1,25 @@
+//
+//  SecondScreenView.swift
+//  alpha_fanta_ge
+//
+//  Created by michael tamsil on 20/12/20.
+//
+
+//    Second Screen should allow user to enter multiple “symbols” to search its Daily Adjusted
+//    data and show its comparison in UITableView. (compare “open” and “low” value based on
+//    date)
+
+import SwiftUI
+
+struct SecondScreenView: View {
+
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SecondScreenView_Previews: PreviewProvider {
+    static var previews: some View {
+        SecondScreenView()
+    }
+}
