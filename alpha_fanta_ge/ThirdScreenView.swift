4017b2d05af8439ce6d2585b2b797a872dabda74
Michael Tamsil <michaeltamsil@gmail.com>
2020-12-21 03:57:39 +0700

    create empty third view

---
+++ alpha_fanta_ge/ThirdScreenView.swift
@@ -0,0 +1,23 @@
+//
+//  ThirdScreenView.swift
+//  alpha_fanta_ge
+//
+//  Created by michael tamsil on 20/12/20.
+//
+
+//Third Screen should allow user to configure API parameters like “API Key”, “interval” and
+//“outputsize” (using UIPickerView/UISegment/UITableView)
+
+import SwiftUI
+
+struct ThirdScreenView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ThirdScreenView_Previews: PreviewProvider {
+    static var previews: some View {
+        ThirdScreenView()
+    }
+}
